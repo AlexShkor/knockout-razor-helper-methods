4aee5e3be1d03db08c348c9b5e57a13f9ac3b126
Alex Shkor <alex.shkor@gmail.com>
2013-01-29 16:07:29 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 knockout-razor-helper-methods
 =============================
+Put KO.cshtml in App_Code folder in web app root. And include StringExt.cs
---
+++ src/StringExt.cs
@@ -0,0 +1,13 @@
+﻿namespace System;
+{
+    /// <summary>
+    /// Extension methods for <see cref="System.String"/>
+    /// </summary>
+    public static class StringExt
+    {
+        public static bool HasValue(this String value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+    }
+}
